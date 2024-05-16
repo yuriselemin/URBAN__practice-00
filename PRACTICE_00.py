e81ad856d9f06cf2288c741829991e6e7fc88210
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-17 00:51:14 +0300

    Add files via upload

---
+++ PRACTICE_00.py
@@ -0,0 +1,62 @@
+#Практическое задание по уроку "Базовые структуры данных"
+
+
+
+# Задача «Длина слова».
+
+# 1 вариант
+num = int(5648989)
+print(len(str(num)))
+# 2 вариант
+print(len(str(5648989)))
+
+
+
+# Задача «Суммы и разности».
+
+num_1 = int(20)
+num_2 = int(10)
+summ = (num_1 + num_2)
+diff = (num_1 - num_2)
+
+print(summ)
+print(diff)
+print(str(summ))
+print(str(diff))
+
+
+
+# Задача «Среднее арифметическогое».
+
+num_1 = int(10)
+num_2 = int(10)
+num_3 = int(10)
+arith_mean = (int((num_1 + num_2 + num_3)/3))
+
+print(str(arith_mean))
+
+
+
+# Задача «Простые строчки».
+
+a = "Вторник"
+b = "Понедельник"
+
+print(b + ", " + a)
+
+
+
+# Задача «Сложная формула».
+
+a = int(2)
+b = int(2)
+c = int(2)
+f = ((a * b) + (a * c))
+
+print(int((f ** 3)/2))
+
+
+
+
+
+
